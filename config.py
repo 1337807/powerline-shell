b6bbbfe687cd256e9f3d9e205d2e7ef8257d84ca
Jonan Scheffler <jonan@heroku.com>
2015-08-13 23:54:52 -0700

    Update config.py

--- config.py
+++ config.py
@@ -8,38 +8,33 @@
 # prompt. Any segment you add must be present in the segments/ directory
 
 SEGMENTS = [
-# Show current virtual environment (see http://www.virtualenv.org/)
-    # 'virtual_env',
+  # Set the terminal window title to user@host:dir
+  'set_term_title',
 
-# Show the current user's username as in ordinary prompts
-    'username',
+  # Show the last command's exit code if it was non-zero
+  'exit_code',
 
-# Show the machine's hostname. Mostly used when ssh-ing into other machines
-    'hostname',
+  # Show a padlock when ssh-ing from another machine
+  'ssh',
 
-# Show the current directory. If the path is too long, the middle part is
-# replaced with ellipsis ('...')
-    'cwd',
+  # Show the current directory. If the path is too long, the middle part is
+  # replaced with ellipsis ('...')
+  'cwd',
 
-# Show the current git branch and status
-    'git',
+  # Show the current git branch and status
+  'git',
 
-# Show the current mercurial branch and status
-    # 'hg',
+  # Show a padlock if the current user has no write access to the current
+  # directory
+  'read_only',
 
-# Show the current svn branch and status
-    # 'svn',
+  # Show number of running jobs
+  'jobs',
 
-# Show the current fossil branch and status
-    # 'fossil',
-
-# Show number of running jobs
-    'jobs',
-
-# Shows a '#' if the current user is root, '$' otherwise
-# Also, changes color if the last command exited with a non-zero error code
-    'root',
-    'ruby_version',
+  # Shows a '#' if the current user is root, '$' otherwise
+  # Also, changes color if the last command exited with a non-zero error code
+  'root',
+  'ruby_version',
 ]
 
 # Change the colors used to draw individual segments in your prompt
